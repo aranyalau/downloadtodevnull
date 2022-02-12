e7579c078eb21b01f4f04f3be550d0e24fe3745d
aranyalau <99336112+aranyalau@users.noreply.github.com>
2022-02-12 23:49:27 +0800

    Update d.sh

--- d.sh
+++ d.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 while [ $((i++)) -le 100 ]
 do echo the $i time;
-wget --no-check-certificate -o /dev/null https://mirror.umd.edu/ubuntu-iso/21.10/ubuntu-21.10-desktop-amd64.iso;
+wget --no-check-certificate -O /dev/null https://mirror.umd.edu/ubuntu-iso/21.10/ubuntu-21.10-desktop-amd64.iso;
 done
