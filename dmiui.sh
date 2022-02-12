0c028beb7379f460711e6bbfe30be62461e28c3d
aranyalau <99336112+aranyalau@users.noreply.github.com>
2022-02-12 23:43:20 +0800

    Create dmiui.sh

---
+++ dmiui.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+while [ $((i++)) -le 100 ]
+do echo the $i time;
+wget --no-check-certificate -o /dev/null https://bigota.d.miui.com/V11.0.5.0.PCACNXM/miui_MI6_V11.0.5.0.PCACNXM_996ffd2660_9.0.zip;
+done
