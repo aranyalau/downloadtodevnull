b98e0073ecbb31b5d9413d4f6126fc7850f8ad3b
aranyalau <99336112+aranyalau@users.noreply.github.com>
2022-02-12 23:48:58 +0800

    Update dmiui.sh

--- dmiui.sh
+++ dmiui.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 while [ $((i++)) -le 100 ]
 do echo the $i time;
-wget --no-check-certificate -o /dev/null https://bigota.d.miui.com/V11.0.5.0.PCACNXM/miui_MI6_V11.0.5.0.PCACNXM_996ffd2660_9.0.zip;
+wget --no-check-certificate -O /dev/null https://bigota.d.miui.com/V11.0.5.0.PCACNXM/miui_MI6_V11.0.5.0.PCACNXM_996ffd2660_9.0.zip;
 done
